ce884c96210c5f6e113173025013d5c213e69e98
santhoshnc <ncsanthosh7@gmail.com>
2023-07-13 21:28:26 +0530

    Day#3 : Added program for RestAPI invocation

---
+++ Day#3/04_restapi.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func main() {
+
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./04_restapi.go <URL>\n")
+		os.Exit(1)
+	}
+
+	//myURL, err := url.ParseRequestURI(args[1])
+	//_, err := url.ParseRequestURI(args[1])
+
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, body)
+
+}
